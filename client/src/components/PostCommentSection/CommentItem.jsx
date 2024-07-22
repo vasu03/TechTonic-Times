00711e9941fc472d187a05a5edd5994f52679a34
vasu03 <vasurmakadia@gmail.com>
2024-07-22 19:48:34 +0530

    [ add, update ] :: Showing all the comments for a post.

---
+++ client/src/components/PostCommentSection/CommentItem.jsx
@@ -0,0 +1,92 @@
+// Imporitng the component
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+
+// Importing icons
+// Importing the Icons
+import { HiOutlineHeart, HiHeart } from "react-icons/hi";
+
+// Creating the CommentItem component
+const CommentItem = ({ comment }) => {
+    // destructuring the incoming data
+    const { postId, userId, content, likes, numberOfLike, createdAt } = comment;
+
+    // Some states to handle the comments & user data
+    const [user, setUser] = useState([]);
+    const [getUserError, setGetUserError] = useState(null);
+
+    // Variables to handle the comments data
+    const commentCreatedAt = moment(createdAt).fromNow();
+
+    // Effect to fetch the user data for every comment
+    useEffect(() => {
+        try {
+            // function to fetch the user data
+            const fetchUser = async () => {
+                // get the response from server
+                const res = await fetch(`/api/user/getCommentUser/${userId}`, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                });
+
+                // convert the obtained data
+                const data = await res.json();
+
+                if (!res.ok) {
+                    setGetUserError(data.message);
+                } else {
+                    setUser(data);
+                }
+            }
+
+            // calling thr funciton for execution
+            fetchUser();
+        } catch (error) {
+            console.log(error.message);
+        }
+    }, [comment]);
+
+    // JSX to render the component
+    return (
+        <div className="w-full flex border-b border-slate-200 dark:border-slate-700 items-start justify-between gap-2 sm:gap-4 mb-3 sm:p-2 px-1 py-2">
+            {/* User profile pic */}
+            <div className="shrink-0 items-center justify-center sm:flex">
+                <img src={user.profilePicture} alt="DP" className="w-10 h-10 sm:w-14 sm:h-14 object-cover rounded-full" />
+            </div>
+            {/* Details regarding comment */}
+            <div className="flex flex-1 flex-col gap-0 justify-center sm:px-2">
+                {/* User name and time of creation and likes */}
+                <div className="w-full flex items-start justify-between">
+                    <div className="flex items-center justify-start gap-2">
+                        <span className="flex flex-1 items-center justify-start font-semibold italic text-sm truncate dark:text-gray-300 text-gray-700">
+                            {user ? `@${user.userName}` : "User"}
+                        </span>
+                        <span className="w-max flex items-center justify-center text-[9px] dark:text-gray-400 text-gray-500 ">
+                            {commentCreatedAt}
+                        </span>
+                    </div>
+                    <div className="w-max flex flex-col-reverse items-center justify-center gap-1 text-[10px] dark:text-gray-400 text-gray-500 ">
+                        {numberOfLike}
+                        <HiOutlineHeart className="text-lg cursor-pointer" />
+                    </div>
+                </div>
+
+                {/* Content of the comment */}
+                <div className="flex flex-col items-start justify-center">
+                    <div className="text-sm dark:text-gray-400 text-gray-500">
+                        {comment.content}
+                    </div>
+                    <div className="flex gap-2 mt-2 py-2 items-center justify-center dark:text-gray-400 text-gray-600 text-xs border-t border-slate-200 dark:border-slate-700">
+                        <span className="">Edit</span>
+                        <span className="">Delete</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+// Exporting the component
+export default CommentItem;
--- client/src/components/PostCommentSection/PostCommentSection.jsx
+++ client/src/components/PostCommentSection/PostCommentSection.jsx
@@ -1,5 +1,5 @@
 // Importing the required modules
-import React, { useState } from "react";
+import React, { memo, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import toast from "react-hot-toast";
@@ -7,15 +7,20 @@ import toast from "react-hot-toast";
 // Importing the ui components
 import { Alert, Button, Textarea } from "flowbite-react";
 
+// Importing custom components
+import CommentItem from "./CommentItem";
+
 
 // Creating the Post Comment Section component
 const PostCommentSection = ({ postId }) => {
     // Retrieve current user information from Redux store global state
     const { currentUser } = useSelector((state) => state.user);
 
-    // Some states to handle the comments
-    const [comment, setComment] = useState("");
-    const [commentError, setCommentError] = useState(null);
+    // Some states to handle the comments for a post
+    const [commentToAdd, setCommentToAdd] = useState("");
+    const [commentToAddError, setCommentToAddError] = useState(null);
+    const [getComments, setGetComments] = useState([]);
+    const [getCommentError, setGetCommentError] = useState(null);
 
     // function to handle the adding of new comments to a post
     const handleAddComment = async (e) => {
@@ -28,7 +33,7 @@ const PostCommentSection = ({ postId }) => {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify({
-                    content: comment,
+                    content: commentToAdd,
                     postId: postId,
                     userId: currentUser._id
                 }),
@@ -39,18 +44,53 @@ const PostCommentSection = ({ postId }) => {
 
             // If the response is not ok then show error
             if (!res.ok) {
-                setCommentError(data.message);
-            } else{
-                setComment("");
+                setCommentToAddError(data.message);
+            } else {
+                setCommentToAdd("");
+                setGetComments([data, ...getComments]);
                 toast.success("Comment added!!");
-                setCommentError(null);
+                setCommentToAddError(null);
             }
         } catch (error) {
-            setCommentError(error.message);
+            setCommentToAddError(error.message);
         }
     }
 
     // function to handle editting of comment
+
+
+    // Effect to handle the fetching of comments whenever post changes
+    useEffect(() => {
+        try {
+            // creating a function for fetching comments
+            const fetchComments = async () => {
+                // get the response from server
+                const res = await fetch(`/api/post/getComment/${postId}`, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                });
+
+                // convert the obtained data
+                const data = await res.json();
+
+                if (!res.ok) {
+                    setGetCommentError(data.message);
+                } else {
+                    setGetComments(data);
+                }
+            }
+
+            // call the function for execution
+            fetchComments();
+
+
+        } catch (error) {
+            console.log(error.message);
+        }
+    }, [postId]);
+
 
     //   JSX to render the component
     return (
@@ -76,30 +116,58 @@ const PostCommentSection = ({ postId }) => {
 
             {/* Input for writting the comment */}
             {currentUser && (
-                <form onSubmit={handleAddComment} className="flex flex-col items-center justify-center gap-2 w-full">
+                <form onSubmit={handleAddComment} className="flex flex-col items-center justify-center gap-2 my-3 border border-gray-500 dark:border-gray-600 p-2 rounded-lg w-full">
                     <Textarea
-                        value={comment}
-                        onChange={(e) => setComment(e.target.value)}
+                        value={commentToAdd}
+                        onChange={(e) => setCommentToAdd(e.target.value)}
                         placeholder="Add a comment..."
                         rows={3}
                         maxLength={200}
                     />
                     <div className="flex items-center justify-between w-full">
-                        <p className="text-xs text-gray-500 dark:text-gray-300">{200 - comment.length} characters remaining</p>
+                        <p className="text-xs text-gray-500 dark:text-gray-300">{200 - commentToAdd.length} characters remaining</p>
                         <Button size={"xs"} gradientMonochrome="cyan" type="submit" className="self-end rounded-md">Comment</Button>
                     </div>
+                    {/* Alert to display updation Failure */}
+                    {commentToAddError !== null && (
+                        <Alert color="failure" className="mt-5">
+                            {commentToAddError}
+                        </Alert>
+                    )}
                 </form>
             )}
 
-            {/* Alert to display updation Failure */}
-            {commentError !== null && (
-                <Alert color="failure" className="mt-5">
-                    {commentError}
-                </Alert>
-            )}
+
+
+            {/* Showing all the comments for a post */}
+            {getComments.length > 0 ?
+                // Show all the comments
+                (
+                    <div className="flex flex-col mt-3 overflow-auto sm:px-2">
+                        {/* Show no. of comments */}
+                        <div className="flex items-center justify-start w-full gap-2 my-2 p-2 text-gray-500 dark:text-gray-300">
+                            <p>Comments</p>
+                            <div className="flex items-center justify-center rounded-md w-6 h-6 dark:bg-gray-600 bg-gray-200 cursor-pointer hover:bg-gray-300 hover:dark:bg-gray-500">{getComments.length}</div>
+                        </div>
+
+                        {/* Show the actual comments */}
+                        <div className="px-1 flex flex-col items-center sm:px-3 h-[30rem] overflow-auto">
+                            {getComments.map((comment) => (
+                                <CommentItem key={comment._id} comment={comment} />
+                            ))}
+                        </div>
+                    </div>
+                ) :
+                // Else show other content
+                (
+                    <div className="flex flex-col w-full items-center justify-center gap-1 text-sm text-gray-500 dark:text-gray-300">
+                        <p>No comments yet.</p>
+                        <p>Be the first one to comment on this post !!</p>
+                    </div>
+                )}
         </div>
     );
 };
 
 // Exporting the component
-export default PostCommentSection;
+export default memo(PostCommentSection);
